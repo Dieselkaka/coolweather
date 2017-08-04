9216a400fac6603ec341962868d9a0e85ef72f10
Diesel <240952819@qq.com>
2017-08-04 15:50:49 +0800

    增加后台自动更新天气的功能.

--- app/src/main/java/com/gisstation/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/gisstation/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.gisstation.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.gisstation.coolweather.android.gosn.Forecast;
 import com.gisstation.coolweather.android.gosn.Weather;
+import com.gisstation.coolweather.android.service.AutoUpdateService;
 import com.gisstation.coolweather.android.util.HttpUtil;
 import com.gisstation.coolweather.android.util.Utility;
 
@@ -226,5 +228,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
        sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
